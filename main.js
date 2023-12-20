021b8bfacfab2ad4f9f08db21046e2999c91ece1
KadekAgusSaputra <aldhypahyana99@gmail.com>
2023-12-20 22:41:34 +0800

    portofolio

---
+++ main.js
@@ -0,0 +1,27 @@
+const sidebar = document.querySelector("#sidebar");
+const buttonToggler = document.querySelector(".navigation__toggler");
+const buttonCloser = document.querySelector(".navigation__closer");
+
+/**
+ * Untuk menampilkan sidebar ketika diklik
+ * 
+ * Cara baca: 
+ * Button toggler ku berikan event ketika di klik, maka di element dengan id sidebar (Tag Aside nya)
+ * tambahkan class tambahan yaitu "show" dan buang class "hide"
+ *  */
+buttonToggler.addEventListener("click", () => {
+  sidebar.classList.toggle("show");
+  sidebar.classList.remove("hide");
+});
+
+/**
+ * Untuk menutup sidebar ketika diklik
+ * 
+ * Cara baca: 
+ * Button closer ku berikan event ketika di klik, maka di element dengan id sidebar (Tag Aside nya)
+ * hapus class tambahan yaitu "hide" dan tambah class "show"
+ *  */
+buttonCloser.addEventListener("click", () => {
+  sidebar.classList.remove("show");
+  sidebar.classList.toggle("hide");
+});
